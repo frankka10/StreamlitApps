e12d05d71abaa4a891ba2f72a7da31a07c5e36bb
Franklin <53950158+frankka10@users.noreply.github.com>
2023-01-19 20:25:04 +0100

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+
+  
 def main():
   df = pd.read_excel("Produits_dopants_20160317.xlsx")
   
-  st.dataframe(df.head())
+  st.title('Drops Substances Analysis')
+  # st.dataframe(df.head(10))
   
 if __name__ == '__main__':
   main()
