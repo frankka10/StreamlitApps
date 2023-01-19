7f931c2e719b8819863979b67b9a930a1f4a416c
Franklin <53950158+frankka10@users.noreply.github.com>
2023-01-19 18:35:56 +0100

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -2,14 +2,10 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-def load_data(data):
-  return pd.read_excel(data)
-
 def main():
-  df = load_data('https://github.com/frankka10/StreamlitApps/blob/main/Produits_dopants_20160317.xlsx')
-  st.title("Drop products")
-  st.dataframe(df.head(10))
+  df = pd.read_excel("Produits_dopants_20160317.xlsx")
   
+  st.dataframe(df.head(10))
   
 if __name__ == '__main__':
   main()
