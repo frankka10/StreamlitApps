4a97ee162bf77a945dba6912d4b351772ab5f4f5
Franklin <53950158+frankka10@users.noreply.github.com>
2023-01-19 19:16:09 +0100

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -5,7 +5,9 @@ import streamlit as st
 def main():
   df = pd.read_excel("Produits_dopants_20160317.xlsx")
   
-  st.dataframe(df.head(10))
+  st.dataframe(df.head())
   
 if __name__ == '__main__':
   main()
+
+  
