9b69b2291697ae2fcdcff0d8afa83161a91a9936
Franklin <53950158+frankka10@users.noreply.github.com>
2023-01-19 18:29:49 +0100

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -2,8 +2,14 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-def main():
-  st.title("Drop products")
+def load_data(data):
+  return pd.read_excel(data)
 
+def main():
+  data = load_data('Produits_dopants_20160317.xlsx')
+  st.title("Drop products")
+  st.dataframe(data.head(10))
+  
+  
 if __name__ == '__main__':
   main()
