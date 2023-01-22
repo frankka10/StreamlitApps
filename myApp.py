f32bccd0a711d8cf74cf18c335b8d39a10fd0c16
Franklin <53950158+frankka10@users.noreply.github.com>
2023-01-22 06:56:11 +0100

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -59,7 +59,7 @@ def main():
   # Classes with the n-highest number of substances
   st.subheader("Overview of the quantity of substances and devirates per classes")
   fig = px.pie(df.head(10), values="Number of Substances", names="Classe", title="aertr")
-  st.plotly_chart(fig)  
+  st.plotly_chart(fig, use_container_width=True)  
   with st.expander("Explanation"):
     st.write("This graph describes the number of substances per class and"
              "the number of deviations of substances per class found in our dataset")
